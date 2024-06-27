4c5a8885cda0ac415e59331b8b6fe388b36242f2
giannisaladino <saladinogianni6@gmail.com>
2024-06-27 17:52:19 +0200

    ADD -> added TypeSeeder

--- final-proj-backend/database/seeders/TypeSeeder.php
+++ final-proj-backend/database/seeders/TypeSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+
 
 class TypeSeeder extends Seeder
 {
@@ -12,6 +16,16 @@ class TypeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $types = ['Italiano', 'Cinese', 'Indiano', 'Giapponese', 'Messicano', 'Pizzeria','Pesce', 'Carne', 'Rosticceria',
+        'Kebab', 'Paninoteca', 'Pokeria', 'Pub', 'Gelateria', 'Yogurteria', 'Hamburger', 'Sushi'];
+
+        foreach ($types as $type_name) {
+            $new_type = new Type();
+            $new_type->name = $type_name;
+            $new_type->slug = Str::slug($type_name);
+
+            $new_type->save();
+        }
+
     }
 }
