f5035c6b59ee2b244ae28a0c48cd621d0bb12a2a
MarcoColloca <marco.colloca3511@gmail.com>
2024-06-27 17:06:58 +0200

    Add → added basic structure for DishSeeder

--- final-proj-backend/database/seeders/DishSeeder.php
+++ final-proj-backend/database/seeders/DishSeeder.php
@@ -2,16 +2,28 @@
 
 namespace Database\Seeders;
 
+use App\Models\Dish;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 class DishSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for($i = 0; $i < 5; $i++)
+        {
+            $new_dish = new Dish();
+
+
+
+
+            dump($new_dish);
+            //$new_dish->save();
+        }
     }
 }
