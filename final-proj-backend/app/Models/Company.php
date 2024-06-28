0b2e963430e8ecaf7fd13c4659dd526c53f42777
Francesca Barletta <wallebar@gmail.com>
2024-06-28 11:08:40 +0200

    ADD: relations one to many user/companies, many to many companies/types

--- final-proj-backend/app/Models/Company.php
+++ final-proj-backend/app/Models/Company.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Company extends Model
 {
     use HasFactory;
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function types(){
+        return $this->belongsToMany(Type::class);
+    }
 }
