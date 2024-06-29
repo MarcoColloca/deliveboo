54c7297d20ac5688f96865b9ff6d410eb099917d
MarcoColloca <marco.colloca3511@gmail.com>
2024-06-29 17:26:14 +0200

    Add → added HasSlug Trait and $fillable to Admin/Dish Model

--- final-proj-backend/app/Models/Dish.php
+++ final-proj-backend/app/Models/Dish.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use App\Http\Traits\HasSlug;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Dish extends Model
 {
-    use HasFactory;
+    use HasFactory, HasSlug;
 
 
     public function company()
@@ -20,5 +21,5 @@ class Dish extends Model
         return $this->belongsToMany(Order::class);
     }
 
-    protected $fillable = ['company_id'];
+    protected $fillable = ['name', 'slug', 'image', 'description', 'ingredients', 'price', 'visible', 'company_id'];
 }
