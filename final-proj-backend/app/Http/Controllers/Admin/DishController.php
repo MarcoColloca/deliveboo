0f5e79099c2450e696973bec9a8074be7b404dd1
MarcoColloca <marco.colloca3511@gmail.com>
2024-06-29 13:01:59 +0200

    Refactor → removed comments

--- final-proj-backend/app/Http/Controllers/Admin/DishController.php
+++ final-proj-backend/app/Http/Controllers/Admin/DishController.php
@@ -12,40 +12,7 @@ class DishController extends Controller
 {
     /**
      * Display a listing of the resource.
-     */
-
-    /*
-    public function index()
-    {
-        $user_id = Auth::id();
-
-        $company_ids = Company::where('user_id', $user_id)->get();
-               
-
-        $company_id = $company_ids->pluck('id');
-
-        
-        $query = Dish::with('company')->orderBy('name', 'asc');
-        
-        if($company_id){
-            $company_names = [];
-            $company_dishes = [];
-
-            foreach ($company_id as $id) {
-                
-                $company_dishes [] = $query->where('company_id', $id)->get();
-                $company_names [] = Company::where('id', $id)->pluck('name');
-            }
-
-            
-        }
-
-
-        return view('admin.dishes.index', compact('company_dishes', 'company_names'));
-    }
-
-    */  
-
+    */
     
     public function index()
     {
