b465ce901a0f970a79d84f53d7f8150724cbe018
MarcoColloca <marco.colloca3511@gmail.com>
2024-07-01 15:35:38 +0200

    Add → added Api dishes/companies routes

--- final-proj-backend/routes/api.php
+++ final-proj-backend/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\CompanyController;
+use App\Http\Controllers\Api\DishController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('companies', [CompanyController::class, 'index']);
+
+Route::get('dishes', [DishController::class, 'index']);
+
+Route::get('dishes/{dish:slug}', [DishController::class, 'show']);
