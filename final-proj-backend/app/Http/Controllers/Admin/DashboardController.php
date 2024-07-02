7125ea684f297d7d2221670a439e784e208ed59b
MarcoColloca <marco.colloca3511@gmail.com>
2024-07-02 20:12:48 +0200

    Add → added DashboardController

--- final-proj-backend/app/Http/Controllers/Admin/DashboardController.php
+++ final-proj-backend/app/Http/Controllers/Admin/DashboardController.php
@@ -3,9 +3,21 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Company;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-    //
+    public function index()
+    {   
+        $user_id = Auth::id();
+
+        $user_name = Auth::user()->name;
+
+        $companies = Company::where('user_id', $user_id)->get();
+
+
+        return view('admin.dashboard', compact('companies', 'user_name'));
+    }
 }
