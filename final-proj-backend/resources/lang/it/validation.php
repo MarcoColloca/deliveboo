0176fe294abbfc775ab0e4810775be8dda899faf
Mustaaaaa <moustafaibrahim1512@gmail.com>
2024-07-02 15:42:29 +0200

    add --> messaggio di errore tradotto in italiano

---
+++ final-proj-backend/resources/lang/it/validation.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'required' => 'Il campo :attribute è obbligatorio.',
+    'email' => 'Il campo :attribute deve essere un indirizzo email valido.',
+    'max' => [
+        'string' => 'Il campo :attribute non può essere più lungo di :max caratteri.',
+    ],
+
+    'attributes' => [
+        'name' => 'nome',
+        'price' => 'prezzo',
+        'ingredients' => 'ingredienti',
+        'description' => 'descrizione',
+        'visible' => 'visibile',
+        'city' => 'città',
+        'phone_number' => 'numero di telefono',
+        'address' => 'indirizzo',
+        'vat_number' => 'P.iva',
+
+
+
+
+    ],
+];
+
+
+
+?>
