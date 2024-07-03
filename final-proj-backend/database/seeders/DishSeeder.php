fade148900d887c26d00a14cb6a51f428544ff3d
Francesca Barletta <wallebar@gmail.com>
2024-07-03 10:25:12 +0200

    Add: reding file json for dish seeder

--- final-proj-backend/database/seeders/DishSeeder.php
+++ final-proj-backend/database/seeders/DishSeeder.php
@@ -8,43 +8,42 @@ use App\Models\Order;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
-
+use Illuminate\Support\Facades\File;
 class DishSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(Faker $faker): void
+    public function run()
     {
+        //leggi file json
+        $json = File::get("database/json/dishes.json");
+        $data = json_decode($json);
 
-        $company_ids = Company::all()->pluck('id')->all();
-        $order_ids = Order::all()->pluck('id')->all();
-        for($i = 0; $i < 5; $i++)
-        {
-            $new_dish = new Dish();
-            $new_dish->name = $faker->sentence(1,5);
-            $new_dish->slug = Str::slug($new_dish->name);
-            $new_dish->image ="https://picsum.photos/300/200?random=".rand(1,300);
-            $new_dish->description = $faker->paragraph(3,15);
-            $new_dish->ingredients = $faker->paragraph(2,10);
-            $new_dish->price = $faker->randomFloat(2,1,200);
-            $new_dish->visible = $faker->boolean();
-            $new_dish->company_id = $faker->randomElement($company_ids);
-            
-            // dump($new_dish);
-            $new_dish->save();
+        foreach($data->dishes as $dish) {
+            //inserisci i dati nella tab dishes
+            $dishId = DB::table('dishes')->insertGetId([
+                'name' => $dish->name,
+                'slug' => $dish->slug,
+                'image' => $dish->image,
+                'description' => $dish->description,
+                'ingredients' => $dish->ingredients,
+                'price' => $dish->price,
+                'visible' => $dish->visible,
+                'company_id' => $dish->company_id
 
-            $random_order_ids = $faker->randomElements($order_ids, rand(1, 3));
+            ]);
 
-            $random_qty = [];
-
-            foreach ($random_order_ids as $order_id) {
-                $random_qty[$order_id] = ['qty' => rand(1, 5)];
+            //inserisci dati nella taella ponte dish_order
+            foreach($dish->order as $orderId) {
+                DB::table('dish_order')->insert([
+                    'dish_id' => $dishId,
+                    'order_id' =>$orderId,
+                ]);
             }
-
-            $new_dish->orders()->attach($random_qty);
-
         }
+
     }
 }
