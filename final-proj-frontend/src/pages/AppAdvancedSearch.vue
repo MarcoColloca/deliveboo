1cde2bbb65e54f7d34c81aaaf537e5710118eb7b
Mustaaaaa <moustafaibrahim1512@gmail.com>
2024-07-03 16:18:09 +0200

    Add --> routerlink per la pagina del menù

--- final-proj-frontend/src/pages/AppAdvancedSearch.vue
+++ final-proj-frontend/src/pages/AppAdvancedSearch.vue
@@ -1,81 +1,81 @@
 <script>
-    import axios from 'axios';
-    import BentoBox from '../components/single-components/general/BentoBox.vue';
+import axios from 'axios';
+import BentoBox from '../components/single-components/general/BentoBox.vue';
 
-    export default {
-        components: {
-            BentoBox
-        },
+export default {
+    components: {
+        BentoBox
+    },
 
-        props: {
-            slug: String
-        },
+    props: {
+        slug: String
+    },
 
-        data() {
-            return {
-                types: [],
-                currentPage: 1,
-                perPage: 99,
-                selectedTypeSlugs: [], 
-                companies: [],
-            }
-        },
+    data() {
+        return {
+            types: [],
+            currentPage: 1,
+            perPage: 99,
+            selectedTypeSlugs: [], 
+            companies: [],
+        }
+    },
 
-        created() {
-            this.fetchTypes();
-            if (this.slug) {
-                this.addSlugToSelectedTypes(this.slug);
-            }
-        },
+    created() {
+        this.fetchTypes();
+        if (this.slug) {
+            this.addSlugToSelectedTypes(this.slug);
+        }
+    },
 
-        watch: {
-            selectedTypeSlugs() {
-                this.fetchCompanies();
-            }
-        },
+    watch: {
+        selectedTypeSlugs() {
+            this.fetchCompanies();
+        }
+    },
 
-        methods: {
-            addSlugToSelectedTypes(slug) {
-                if (!this.selectedTypeSlugs.includes(slug)) {
-                    this.selectedTypeSlugs.push(slug);
-                    this.fetchCompanies(); 
-                }
-            },
-
-            toggleTypeSelection(slug) {
-                if (this.selectedTypeSlugs.includes(slug)) {
-                    this.selectedTypeSlugs = this.selectedTypeSlugs.filter(typeSlug => typeSlug !== slug);
-                } else {
-                    this.selectedTypeSlugs.push(slug);
-                }
+    methods: {
+        addSlugToSelectedTypes(slug) {
+            if (!this.selectedTypeSlugs.includes(slug)) {
+                this.selectedTypeSlugs.push(slug);
                 this.fetchCompanies(); 
-            },
+            }
+        },
 
-            fetchTypes() {
-                axios.get('http://127.0.0.1:8000/api/types', {
-                    params: {
-                        page: this.currentPage,
-                        perPage: this.perPage,
-                    }
-                })
+        toggleTypeSelection(slug) {
+            if (this.selectedTypeSlugs.includes(slug)) {
+                this.selectedTypeSlugs = this.selectedTypeSlugs.filter(typeSlug => typeSlug !== slug);
+            } else {
+                this.selectedTypeSlugs.push(slug);
+            }
+            this.fetchCompanies(); 
+        },
+
+        fetchTypes() {
+            axios.get('http://127.0.0.1:8000/api/types', {
+                params: {
+                    page: this.currentPage,
+                    perPage: this.perPage,
+                }
+            })
                 .then(res => {
                     this.types = res.data.results.data;
                 });
-            },
+        },
 
-            fetchCompanies() {
-                axios.post('http://127.0.0.1:8000/api/types/select', {
+        fetchCompanies() {
+            axios.post('http://127.0.0.1:8000/api/types/select', {
                 typeSlugs: this.selectedTypeSlugs
-                })
+            })
                 .then(res => {
                     this.companies = res.data.results.companies;
                 })
                 .catch(error => {
                     console.error(error);
                 });
-            }
         }
     }
+}
 </script>
 
 
@@ -85,7 +85,8 @@
         <div class="sidebar">
             <ul>
                 <li v-for="type in types" :key="type.slug">
-                    <p @click="toggleTypeSelection(type.slug)" :class="{ selected: selectedTypeSlugs.includes(type.slug) }">
+                    <p @click="toggleTypeSelection(type.slug)"
+                        :class="{ selected: selectedTypeSlugs.includes(type.slug) }">
                         {{ type.name }}
                     </p>
                 </li>
@@ -99,8 +100,8 @@
                 <div class="row row-gap-5">
                     <div class="col-3" v-for="company in companies" :key="company.id">
                         <div class="card">
-                            <div class="card-header">                                
-                                <div class="card-top-img">                                    
+                            <div class="card-header">
+                                <div class="card-top-img">
                                     <img v-if="company.image_fullpath" :src="company.image_fullpath" alt="">
                                     <img v-else src="http://127.0.0.1:8000/storage/image/default-company.jpg" alt="">
                                 </div>
@@ -110,8 +111,12 @@
                                 <p>{{ company.address }}</p>
                                 <p>{{ company.phone_number }}</p>
                                 <p>{{ company.email }}</p>
-                                <span class="me-2 text-danger" v-for="type in company.types" :key="type.id">{{ type.name }}</span>
+                                <span class="me-2 text-danger" v-for="type in company.types" :key="type.id">{{ type.name
+                                    }}</span>
                             </div>
+                            <RouterLink :to="{ name: 'menu', params: { slug: company.slug } }">
+                                Vai al menù
+                            </RouterLink>
                         </div>
                     </div>
                 </div>
@@ -123,44 +128,51 @@
 
 
 <style lang="scss" scoped>
-    .search-container {
-        height: 100%;
-        display: flex;
-        .sidebar {
-            width: 300px;
-            flex-shrink: 0;
-            background-color: rgb(168, 194, 185);
-            text-align: center;
-            padding-top: 30px;
-            ul {
-                padding: 0;
-                li {
-                    display: flex;
-                    width: 100%;
-                    justify-content: center;
-                    p {
-                        cursor: pointer;
-                        width: max-content;
-                    }
-                    p.selected {
-                        font-weight: bold;
-                        color: coral;
-                        background-color: white;
-                        border: 1px solid white;
-                        border-radius: 18px;
-                        padding: 0 3px;
-                    }
+.search-container {
+    height: 100%;
+    display: flex;
+
+    .sidebar {
+        width: 300px;
+        flex-shrink: 0;
+        background-color: rgb(168, 194, 185);
+        text-align: center;
+        padding-top: 30px;
+
+        ul {
+            padding: 0;
+
+            li {
+                display: flex;
+                width: 100%;
+                justify-content: center;
+
+                p {
+                    cursor: pointer;
+                    width: max-content;
+                }
+
+                p.selected {
+                    font-weight: bold;
+                    color: coral;
+                    background-color: white;
+                    border: 1px solid white;
+                    border-radius: 18px;
+                    padding: 0 3px;
                 }
             }
         }
-        .content {
-            text-align: center;
-            flex-grow: 1;
-            background-color: rgb(184, 196, 206);
-            color: black;
-            img{
-                max-height: 181px;
-            }
+    }
+
+    .content {
+        text-align: center;
+        flex-grow: 1;
+        background-color: rgb(184, 196, 206);
+        color: black;
+
+        img {
+            max-height: 181px;
         }
     }
+}
 </style>
