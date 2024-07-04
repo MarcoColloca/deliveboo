253e1c647e5f2a07d30024bbd70b9e899b40f02e
MarcoColloca <marco.colloca3511@gmail.com>
2024-07-04 21:26:41 +0200

    Add → aggiunti i proxy per il server di BrainTree

--- final-proj-frontend/vite.config.js
+++ final-proj-frontend/vite.config.js
@@ -3,15 +3,22 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  
-  resolve: {
-    alias: [
-        {
-            // this is required for the SCSS modules
-            find: /^~(.*)$/,
-            replacement: '$1',
-        },
-    ],
-},
+    plugins: [vue()],
+
+    server: {
+        proxy: {
+          '/token': 'http://localhost:3000',
+          '/checkout': 'http://localhost:3000'
+        }
+    },
+    
+    resolve: {
+        alias: [
+            {
+                // this is required for the SCSS modules
+                find: /^~(.*)$/,
+                replacement: '$1',
+            },
+        ],
+    },
 })
