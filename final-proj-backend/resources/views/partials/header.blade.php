bd569e3b3fd4f8b0cb1343e7ff6f9f322093ab6b
Francesca Barletta <wallebar@gmail.com>
2024-07-05 09:27:36 +0200

    refactor height to hero section home front office

--- final-proj-backend/resources/views/partials/header.blade.php
+++ final-proj-backend/resources/views/partials/header.blade.php
@@ -1,6 +1,7 @@
 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
     <div class="container">
         <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
+            
             <div class="my-logo">
                 <img src="{{Vite::asset('resources/img/logo.png')}}" alt="">
             </div>
