9b2ce5693d083a8aec4afd568b20c1b85755ebab
MarcoColloca <marco.colloca3511@gmail.com>
2024-07-08 00:04:38 +0200

    Add → aggiunta la pagina showOne per mostrare gli ordini di un singolo ristorante

---
+++ final-proj-backend/resources/views/admin/orders/showOne.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.app')
+
+@section('title', 'Ordini per ' . $company->name)
+
+@section('content')
+<section class="my-3 py-1">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="d-flex justify-content-between align-items-end mt-5 mb-1">
+                    <h3 class="text-light">
+                        <p class="text-blue">
+                            {{ $company->name }}
+                            <span class="fs-6 ms-3">{{ $company->address }}</span>
+                        </p>
+                    </h3>
+                </div>
+                <table class="table">
+                    <thead>
+                            <tr>
+                                <th scope="col">Nome Cliente</th>
+                                <th class="text-start" scope="col">Indirizzo</th>
+                                <th class="text-start" scope="col">Email</th>
+                                <th class="text-start" scope="col">Telefono</th>
+                                <th class="text-center" scope="col">Totale Ordine</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            @foreach ($orders as $order)
+                                <tr class="position-relative">
+                                    <td class="text-start fw-lighter">{{ $order->customer_name }}</td>
+                                    <td class="text-start fw-lighter">{{ $order->customer_address }}</td>
+                                    <td class="text-start fw-lighter">{{ $order->customer_mail ?? 'nessuna mail'}}</td>
+                                    <td class="text-start fw-lighter">{{ $order->customer_phone}}</td>
+                                    <td class="text-center fw-lighter">{{ $order->total}}  €</td>
+                                    <td class="text-center">
+                                        <a href="{{ route('admin.orders.show', $order) }}" class="link link-success">Dettagli</a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
